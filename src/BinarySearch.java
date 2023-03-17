20966b9d5cf511529320aa4c2e91fd51a2301e89
Michael Brover <lepovan1999@gmail.com>
2023-03-17 02:02:21 +0200

    added new solutions

---
+++ src/BinarySearch.java
@@ -0,0 +1,21 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,7,10,12};
+        System.out.println(binarySearch(arr, 0, arr.length - 1, 7));
+    }
+    public static int binarySearch(int[] arr, int l, int r, int x)
+    {
+        if (r >= l) {
+            int mid = l + ((r - l) / 2);
+            if (arr[mid] == x)
+                return mid;
+            
+            if (arr[mid] > x)
+                return binarySearch(arr, l, mid - 1, x);
+
+            return binarySearch(arr, mid + 1, r, x);
+        }
+        return -1;
+    }
+}
+
